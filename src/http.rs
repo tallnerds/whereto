752d1c48243254634319aa4de38eace84def444b
Matt McFarland <783024+vanetix@users.noreply.github.com>
2021-10-31 22:58:36 -0500

    feat: add initial working version

--- src/http.rs
+++ src/http.rs
@@ -1,5 +1,57 @@
-pub async fn resolve_url(s: String) -> Result<String, reqwest::Error> {
-  let response = reqwest::get(s).await?.status();
+use futures::future::{BoxFuture, FutureExt};
+use futures::stream::{self, StreamExt};
+use reqwest::{Client, Url};
+use std::collections::HashMap;
+use std::sync::Arc;
 
-  Ok(format!("{:?}", response.to_string()))
+const POOL_SIZE: usize = 5;
+
+pub struct Processor {
+    pub hosts: Vec<Url>,
+    client: Client,
+}
+
+impl Processor {
+    pub fn from_hosts(hosts: Vec<Url>) -> Self {
+        Processor {
+            hosts,
+            client: Client::new(),
+        }
+    }
+
+    pub async fn process(&self) -> Result<HashMap<Url, Url>, reqwest::Error> {
+        let client = Arc::new(self.client.clone());
+
+        let results = stream::iter(self.hosts.clone())
+            .map(move |h| {
+                let client = client.clone();
+
+                // TODO: Fix this unwrap, probably need a custom error
+                async move { resolve_url(client, h).await.unwrap() }
+            })
+            .buffered(POOL_SIZE)
+            .collect::<Vec<Url>>()
+            .await;
+
+        let hashmap: HashMap<Url, Url> = self
+            .hosts
+            .iter()
+            .zip(results)
+            .filter(|(src, dest)| src != &dest)
+            .fold(HashMap::new(), |mut map, (o, r)| {
+                map.insert(o.to_owned(), r);
+                map
+            });
+
+        Ok(hashmap)
+    }
+}
+
+fn resolve_url(client: Arc<Client>, url: Url) -> BoxFuture<'static, Result<Url, reqwest::Error>> {
+    async move {
+        let response = client.get(url.clone()).send().await?;
+
+        Ok(response.url().to_owned())
+    }
+    .boxed()
 }
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,15 @@
-use futures::executor::block_on;
 use reqwest::Url;
+use std::collections::HashMap;
+use std::fs::File;
+use std::io::BufRead;
+use std::io::BufReader;
 use std::path::PathBuf;
 use std::str::FromStr;
 use structopt::clap;
 use structopt::StructOpt;
+use tabular::Row;
+use tabular::Table;
+use tokio::runtime::Runtime;
 
 mod http;
 
@@ -37,21 +43,66 @@ struct Opts {
 
     /// can use if just checking a single url
     #[structopt(short, long)]
-    url: Option<Url>,
+    url: Option<Vec<Url>>,
 }
 
 fn main() {
     let opts = Opts::from_args();
 
-    let results: Vec<String> = if let Some(url) = opts.url {
-        let thing = http::resolve_url(url.into());
-
-        let t = block_on(thing);
-
-        println!("{:?}", t);
-
-        vec![]
+    let hosts: Vec<Url> = if let Some(urls) = opts.url {
+        urls
+    } else if let Some(input) = opts.input {
+        read_hosts_from_file(input).unwrap()
     } else {
-        vec![]
+        panic!("yikes");
     };
+
+    let processor = http::Processor::from_hosts(hosts);
+    let runtime = Runtime::new().unwrap();
+    let results = runtime.block_on(processor.process());
+
+    if let Ok(urls) = results {
+        let output = match opts.output {
+            OutputFormat::JSON => {
+                let urls = urls
+                    .iter()
+                    .map(|(src, dest)| {
+                        HashMap::<String, String>::from_iter(
+                            [
+                                ("source".to_string(), src.to_string()),
+                                ("redirect".to_string(), dest.to_string()),
+                            ]
+                            .into_iter(),
+                        )
+                    })
+                    .collect::<Vec<_>>();
+
+                serde_json::to_string(&urls).unwrap()
+            }
+
+            OutputFormat::Text => urls
+                .iter()
+                .fold(Table::new("{:<}    {:<}"), |mut table, (src, dest)| {
+                    table.add_row(Row::new().with_cell(src).with_cell(dest));
+                    table
+                })
+                .to_string(),
+        };
+
+        println!("{}", output)
+    } else {
+        println!("Failed to fetch urls: {:?}", results)
+    }
+}
+
+fn read_hosts_from_file(path: PathBuf) -> Result<Vec<Url>, std::io::Error> {
+    let file = File::open(path)?;
+    let buf = BufReader::new(file).lines();
+
+    buf.filter(|l| l.is_ok())
+        .map(|l| {
+            Url::from_str(&l.unwrap())
+                .map_err(|e| std::io::Error::new(std::io::ErrorKind::InvalidInput, e))
+        })
+        .collect()
 }
